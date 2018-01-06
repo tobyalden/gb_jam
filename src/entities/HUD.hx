0b1466279faddf3452134c2854203101232b8718
Toby Alden <toby.alden@gmail.com>
2018-01-06 13:18:55 -0800

    Bugfix HUD

--- src/entities/HUD.hx
+++ src/entities/HUD.hx
@@ -14,6 +14,9 @@ class HUD extends Entity
     private var sprite:Spritemap;
     private var text:Text;
     private var textDropshadow:Text;
+    private var textDropshadow2:Text;
+    private var textDropshadow3:Text;
+    private var textDropshadow4:Text;
     private var allGraphics:Graphiclist;
     private var player:Player;
 
@@ -28,6 +31,7 @@ class HUD extends Entity
       super(0, 0);
 
       sprite = new Spritemap("graphics/hearts.png", 90, 9);
+      sprite.smooth = false;
       sprite.add("0", [0]);
       sprite.add("1", [1]);
       sprite.add("2", [2]);
@@ -41,29 +45,53 @@ class HUD extends Entity
       sprite.add("10", [10]);
 
       text = new Text();
+      text.smooth = false;
       text.addStyle("default", {color: 0x072103, size: 8, bold: false});
       text.setTextProperty('richText', true);
       text.y = 110;
       text.x = -5;
 
       textDropshadow = new Text();
+      textDropshadow.smooth = false;
       textDropshadow.addStyle("default", {color: 0xDAEFD7, size: 8, bold: true});
       textDropshadow.setTextProperty('richText', true);
-      textDropshadow.y = 110;
+      textDropshadow.y = 110 + 1;
       textDropshadow.x = text.x;
+
+      textDropshadow2 = new Text();
+      textDropshadow2.smooth = false;
+      textDropshadow2.addStyle("default", {color: 0xDAEFD7, size: 8, bold: true});
+      textDropshadow2.setTextProperty('richText', true);
+      textDropshadow2.y = 110 - 1;
+      textDropshadow2.x = text.x;
+
+      textDropshadow3 = new Text();
+      textDropshadow3.smooth = false;
+      textDropshadow3.addStyle("default", {color: 0xDAEFD7, size: 8, bold: true});
+      textDropshadow3.setTextProperty('richText', true);
+      textDropshadow3.y = 110;
+      textDropshadow3.x = text.x - 1;
+
+      textDropshadow4 = new Text();
+      textDropshadow4.smooth = false;
+      textDropshadow4.addStyle("default", {color: 0xDAEFD7, size: 8, bold: true});
+      textDropshadow4.setTextProperty('richText', true);
+      textDropshadow4.y = 110;
+      textDropshadow4.x = text.x + 1;
 
       prevMessage = "";
       currentMessage = "";
       messageTimer = new GameTimer(MESSAGE_DURATION);
 
       gameOverScreen = new Image("graphics/gameover.png");
+      gameOverScreen.smooth = false;
       gameOverScreen.x = -10;
       gameOverScreen.y = -10;
       gameOverScreen.visible = false;
 
       hud = this;
 
-      allGraphics = new Graphiclist([sprite, textDropshadow, text, gameOverScreen]);
+      allGraphics = new Graphiclist([sprite, textDropshadow, textDropshadow2, textDropshadow3, textDropshadow4, text, gameOverScreen]);
       graphic = allGraphics;
       layer = -999999;
     }
@@ -110,6 +138,9 @@ class HUD extends Entity
 
       text.richText = "<default>" + prevMessage + "\n" + currentMessage + "</default>";
       textDropshadow.richText = "<default>" + prevMessage + "\n" + currentMessage + "</default>";
+      textDropshadow2.richText = "<default>" + prevMessage + "\n" + currentMessage + "</default>";
+      textDropshadow3.richText = "<default>" + prevMessage + "\n" + currentMessage + "</default>";
+      textDropshadow4.richText = "<default>" + prevMessage + "\n" + currentMessage + "</default>";
 
       super.update();
     }
